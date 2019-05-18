b5fabe0034ae9ad77327978477a2a1d13e66e986
endritbeqa18 <45493292+endritbeqa18@users.noreply.github.com>
2019-05-18 16:24:20 +0200

    Update Input.java

--- Input.java
+++ Input.java
@@ -6,7 +6,7 @@ public class Input
  
  public String getCoordinates(String s)//qel nje dritare te re dhe merr informata
  {String rez=j.showInputDialog(s);
-
+  if (rez.toLowerCase().equals("quit")){System.exit(0);}//nese nuk don me lujt shkruan quit
   if(rez==null || rez.length()<3 || rez.trim().length()>5){invalid("Input error");
                                                            return getCoordinates(s);}
   if(!rez.matches("[0-9][0-9 ]*$") ){invalid("Input error");//pranon vetem numra dhe hapsira
